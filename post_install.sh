0856d42ae6619393db391e9b4131468fef1957c4
sjakur <reda-chakkour@hotmail.com>
2019-12-26 00:23:39 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-sudo usermod -aG sudo $1
+echo hello $1
 
-sudo apt-get update
+#sudo usermod -aG sudo $1
 
-sudo apt-get install default-jre
+#sudo apt-get update
 
-sudo apt-get install default-jdk
+#sudo apt-get install default-jre
+
+#sudo apt-get install default-jdk
