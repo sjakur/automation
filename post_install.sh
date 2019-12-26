53369b7de62c861316d85a0a8c75f7b35f41f4d7
sjakur <reda-chakkour@hotmail.com>
2019-12-26 01:04:42 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -6,8 +6,10 @@ sudo usermod -aG sudo $1
 
 sudo apt-get -qq update
 
+sudo apt-get install -qq openssh-server
+
 sudo apt-get install -qq default-jre
 
 sudo apt-get install -qq default-jdk
 
-sudo apt-get install -qq openssh-server
+
