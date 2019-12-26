ccbdec6083574b5c92f2a39772496a4e72c66e85
sjakur <reda-chakkour@hotmail.com>
2019-12-26 18:04:00 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -2,17 +2,15 @@
 
 set -e
 
-echo $1
-echo ---------------------------
 su -c 'usermod -aG sudo '"$1"'' root
 
-#su -c 'apt-get -q -y update' root
+su -c 'apt-get -q -y update' root
 
-#su -c 'apt-get install -q -y openssh-server' root
+su -c 'apt-get install -q -y openssh-server' root
 
-#su -c 'apt-get install -q -y default-jre' root
+su -c 'apt-get install -q -y default-jre' root
 
-#su -c 'apt-get install -q -y default-jdk' root
+su -c 'apt-get install -q -y default-jdk' root
 
-#su -c 'reboot' root
+su -c 'reboot' root
 
